648c8649bdb3032dcb499a2c81fb64803ec4e9ce
YevgenyDrozdov <Yevgeny_Drozdov1@epam.com>
2015-04-24 17:30:59 +0300

    Changes in API

--- src/main/java/com/epam/jmp/gamebox/Action.java
+++ src/main/java/com/epam/jmp/gamebox/Action.java
@@ -1,9 +1,12 @@
 package com.epam.jmp.gamebox;
 
+import java.util.Map;
+
 public interface Action {
 
     String getActionId();
-    String[] getActionParameters();
+    Map<String, String> getActionParametersMap();
+    String getActionParameter(String parameterName);
     Session getSession();
 
 }
